c95c7e7224c4cf51f7f3b96d35306df94c146e98
Estephe fath <estephefath@gmail.com>
2025-06-10 10:19:46 +0200

    add baz

--- src/main/java/com.jad/Foo.java
+++ src/main/java/com.jad/Foo.java
@@ -1,10 +1,22 @@
 package com.jad;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Foo {
     private final Bar bar;
+    private final ArrayList<Baz> bazs = new ArrayList<>();
 
     public Foo(final Bar bar) {
         this.bar = bar;
+    }
+
+    public void addBaz(final Baz baz) {
+        this.bazs.add(baz);
+    }
+
+    public List<Baz> getBaz() {
+        return new ArrayList<>(this.bazs);
     }
 
     public Bar getBar() {
